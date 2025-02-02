7ae5ba9ee3bf617a25c4126b57c47a73c6561b18
sebdal <semoda@bio.aau.dk>
2025-02-02 08:30:30 +0000

    domain library test

--- src/domain/coordinates.rs
+++ src/domain/coordinates.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, Result};
 
 #[derive(Clone, Debug, PartialEq)]
-struct Longitude(f64);
+pub struct Longitude(f64);
 
 impl Longitude {
     pub fn new(value: f64) -> Result<Self> {
@@ -13,16 +13,14 @@ impl Longitude {
 
         Ok(Self(rounded))
     }
+
+    pub fn value(&self) -> f64 {
+        self.0
+    }
 }
 
-// impl fmt::Debug for Longitude {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{}", self.0)
-//     }
-// }
-
 #[derive(Clone, Debug, PartialEq)]
-struct Latitude(f64);
+pub struct Latitude(f64);
 
 impl Latitude {
     pub fn new(value: f64) -> Result<Self> {
@@ -34,13 +32,10 @@ impl Latitude {
 
         Ok(Self(rounded))
     }
+    pub fn value(&self) -> f64 {
+        self.0
+    }
 }
-
-// impl fmt::Debug for Latitude {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{}", self.0)
-//     }
-// }
 
 /// A domain type representing latitude/longitude coordinates.
 ///
@@ -56,18 +51,61 @@ impl Coordinates {
     /// Create new coordinate
     ///
     /// ```
-    /// use domain::Coordinates;
+    /// use weathers::domain::{Coordinates};
     ///
-    /// let coord = Coordinates::new(60, 0).unwrap();
-    /// assert_eq!(coord.longitude, 60.0);
-    ///
-    /// let coord_err = Coordinates::new(0, 91);
-    /// assert!(coord_err.is_err());
+    /// let coord = Coordinates::new(60, 0.0).unwrap();
+    /// assert_eq!(coord.longitude.value(), 60.0);
     /// ```
-    pub fn new(lon: f64, lat: f64) -> Result<Self> {
+    pub fn new<L: Into<f64>, T: Into<f64>>(lon: L, lat: T) -> Result<Self> {
+        let lon = lon.into();
+        let lat = lat.into();
         Ok(Self {
             latitude: Latitude::new(lat)?,
             longitude: Longitude::new(lon)?,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::coordinates::Latitude;
+
+    use super::{Coordinates, Longitude};
+
+    #[test]
+    fn test_boundary_longitude() {
+        let upper = Longitude::new(180.0).unwrap();
+        assert_eq!(upper.value(), 180.0);
+
+        let lower = Longitude::new(-180.0).unwrap();
+        assert_eq!(lower.value(), -180.0);
+
+        let upper_err = Longitude::new(181.0);
+        assert!(upper_err.is_err());
+
+        let lower_err = Longitude::new(-181.0);
+        assert!(lower_err.is_err());
+    }
+
+    #[test]
+    fn test_boundary_latitude() {
+        let upper = Latitude::new(90.0).unwrap();
+        assert_eq!(upper.value(), 90.0);
+
+        let lower = Latitude::new(-90.0).unwrap();
+        assert_eq!(lower.value(), -90.0);
+
+        let upper_err = Latitude::new(91.0);
+        assert!(upper_err.is_err());
+
+        let lower_err = Latitude::new(-91.0);
+        assert!(lower_err.is_err());
+    }
+
+    #[test]
+    fn test_decimal_rounding() {
+        let coord = Coordinates::new(60.10002, 51.00056).unwrap();
+        assert_eq!(coord.longitude.value(), 60.1000);
+        assert_eq!(coord.latitude.value(), 51.0006);
     }
 }
